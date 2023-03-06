c788e02383a8b8246acfb9cc7941f2ea58558ec6
Durga Prasad Mishra <durg0001@edu.zealand.dk>
2023-03-07 00:35:12 +0100

    Add project files.

---
+++ UnitTest/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UnitTest
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+
+        public void ValidatId() 
+        {
+            if (Id == null)
+            {
+                throw new ArgumentOutOfRangeException();
+            }     
+            if (Id == 1) 
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public void ValidatModel()
+        {
+            if (Model == null)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (Model.Length < 4)
+            {
+                throw new ArgumentException();
+            }
+        }
+
+
+        public void ValidationPrice()
+        {
+            if (Price < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if(Price > 0)
+            {
+                throw new ArgumentException();
+            }
+        }
+    } 
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a99a6c41-3fb8-4e63-ba0d-523f3a6109ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
